2e736aa34d49d385c2f556a54206f1ee83fd01f2
JebrilCzar <jgmacanas@up.edu.ph>
2024-03-06 03:54:57 +0800

    ESnamed first commit

---
+++ ESnamed/index.js
@@ -0,0 +1,8 @@
+import { add, PI } from './math.js'
+// importing named exports requires
+//curly braces.)
+// It has to match the name of the
+//import
+// You don’t need to get all of them
+console.log(add(5, 2)); // 7
+console.log(PI); // 3.14
